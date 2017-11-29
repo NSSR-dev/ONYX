8bc0e187eabb6b7ae49e176230b4042dbe79f5a6
Noel <noelshimali@gmail.com>
2017-11-29 22:02:32 +0000

    Auto stash before merge of "master" and "origin/master"

---
+++ src/arduino/_libs/adafruit_bme280_library_modded/examples/bme280test/bme280test.ino
@@ -0,0 +1,93 @@
+/***************************************************************************
+  This is a library for the BME280 humidity, temperature & pressure sensor
+
+  Designed specifically to work with the Adafruit BME280 Breakout
+  ----> http://www.adafruit.com/products/2650
+
+  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
+  to interface. The device's I2C address is either 0x76 or 0x77.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit andopen-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280_mod.h>
+
+#define BME_ASSIGNMENT 0
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+
+#define SN74HC595N_SER 8
+#define SN74HC595N_RCLOCK 7
+#define SN74HC595N_SRCLOCK 2
+
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+//I2C HAS BEEN REMOVED!!!
+
+
+// hardware SPI
+Adafruit_BME280_sn74hc595n bme(BME_ASSIGNMENT, SN74HC595N_SER, SN74HC595N_SRCLOCK, SN74HC595N_RCLOCK);
+
+// software SPI
+//Adafruit_BME280_sn74hc595n bme(BME_ASSIGNMENT, SN74HC595N_SER, SN74HC595N_SRCLOCK, SN74HC595N_RCLOCK, BME_MOSI, BME_MISO, BME_SCK);
+
+
+unsigned long delayTime;
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println(F("BME280 test"));
+
+    bool status;
+    
+    // default settings
+    status = bme.begin();
+    if (!status) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring!");
+        while (1);
+    }
+    
+    Serial.println("-- Default Test --");
+    delayTime = 1000;
+
+    Serial.println();
+}
+
+
+void loop() { 
+    printValues();
+    delay(delayTime);
+}
+
+
+void printValues() {
+    Serial.print("Temperature = ");
+    Serial.print(bme.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print("Pressure = ");
+
+    Serial.print(bme.readPressure() / 100.0F);
+    Serial.println(" hPa");
+
+    Serial.print("Approx. Altitude = ");
+    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+    Serial.println(" m");
+
+    Serial.print("Humidity = ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
+
+    Serial.println();
+}
