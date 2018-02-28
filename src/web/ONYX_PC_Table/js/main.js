fa8dccfff153ec959212d6416d08c77c8b3264db
Noel <noelshimali@gmail.com>
2018-02-28 21:36:00 +0000

    LEDs manual control  wireframe

--- src/web/ONYX_PC_Table/js/main.js
+++ src/web/ONYX_PC_Table/js/main.js
@@ -82,8 +82,8 @@ $tabs.on('click', 'a', function (e) {
 });
 
 
-
-
+/*
+//----------------------LEDS MANUAL
 
 
 
@@ -119,11 +119,32 @@ if( "ontouchstart" in window ) {
 }
 */
 
-var ledRedSlider = document.getElementById('led-red-slider');
 
-ledRedSlider.style.height = '5vw';
-ledRedSlider.style.width = '60vw';
-ledRedSlider.style.margin = '10vw 10vw 30px';
+var ledRedSlider = document.getElementById('led-red-slider');
+var ledGreenSlider = document.getElementById('led-green-slider');
+var ledBlueSlider = document.getElementById('led-blue-slider');
+var ledWhiteSlider = document.getElementById('led-white-slider');
+var ledBrightnessSlider = document.getElementById('led-brightness-slider');
+
+ledRedSlider.style.height = '20px';
+ledRedSlider.style.width = '400px';
+ledRedSlider.style.margin = '8vw 0px 30px';
+
+ledGreenSlider.style.height = '20px';
+ledGreenSlider.style.width = '400px';
+ledGreenSlider.style.margin = '10vw 0px 30px';
+
+ledBlueSlider.style.height = '20px';
+ledBlueSlider.style.width = '400px';
+ledBlueSlider.style.margin = '10vw 0px 30px';
+
+ledWhiteSlider.style.height = '20px';
+ledWhiteSlider.style.width = '400px';
+ledWhiteSlider.style.margin = '10vw 0px 30px';
+
+ledBrightnessSlider.style.height = '300px';
+ledBrightnessSlider.style.width = '20px';
+ledBrightnessSlider.style.margin = '60px 50px 30px';
 
 noUiSlider.create(ledRedSlider, {
 	start: [ 0 ], // 1 handle, starting at...
@@ -147,4 +168,151 @@ noUiSlider.create(ledRedSlider, {
 		density: 5
 	}
 });
+
+noUiSlider.create(ledGreenSlider, {
+	start: [ 0 ], // 1 handle, starting at...
+	margin: 0, // Handles must be at least 300 apart
+	connect: true, // Display a colored bar between the handles
+	direction: 'ltr', // Put '0' at the bottom of the slider
+	orientation: 'horizontal', 
+	behaviour: 'tap-drag', // Move handle on tap, bar is draggable
+	step: 1,
+	tooltips: true,
+	format: wNumb({
+		decimals: 0
+	}),
+	range: {
+		'min': 0,
+		'max': 255
+	},
+	pips: { // Show a scale with the slider
+		mode: 'range',
+		stepped: true,
+		density: 5
+	}
+});
+
+noUiSlider.create(ledBlueSlider, {
+	start: [ 0 ], // 1 handle, starting at...
+	margin: 0, // Handles must be at least 300 apart
+	connect: true, // Display a colored bar between the handles
+	direction: 'ltr', // Put '0' at the bottom of the slider
+	orientation: 'horizontal', 
+	behaviour: 'tap-drag', // Move handle on tap, bar is draggable
+	step: 1,
+	tooltips: true,
+	format: wNumb({
+		decimals: 0
+	}),
+	range: {
+		'min': 0,
+		'max': 255
+	},
+	pips: { // Show a scale with the slider
+		mode: 'range',
+		stepped: true,
+		density: 5
+	}
+});
+
+noUiSlider.create(ledWhiteSlider, {
+	start: [ 0 ], // 1 handle, starting at...
+	margin: 0, // Handles must be at least 300 apart
+	connect: true, // Display a colored bar between the handles
+	direction: 'ltr', // Put '0' at the bottom of the slider
+	orientation: 'horizontal', 
+	behaviour: 'tap-drag', // Move handle on tap, bar is draggable
+	step: 1,
+	tooltips: true,
+	format: wNumb({
+		decimals: 0
+	}),
+	range: {
+		'min': 0,
+		'max': 255
+	},
+	pips: { // Show a scale with the slider
+		mode: 'range',
+		stepped: true,
+		density: 5
+	}
+});
+
+noUiSlider.create(ledBrightnessSlider, {
+	start: [ 50 ], // 1 handle, starting at...
+	margin: 0, // Handles must be at least 300 apart
+	connect: true, // Display a colored bar between the handles
+	direction: 'rtl', // Put '0' at the bottom of the slider
+	orientation: 'vertical', 
+	behaviour: 'tap-drag', // Move handle on tap, bar is draggable
+	step: 1,
+	tooltips: true,
+	format: wNumb({
+		decimals: 0
+	}),
+	range: {
+		'min': 0,
+		'max': 100
+	},
+	pips: { // Show a scale with the slider
+		mode: 'range',
+		stepped: true,
+		density: 10
+	}
+});
+
+
+
+
+function ledStripButtonManual(red, green, blue, white) {
+	ledRedSlider.noUiSlider.set(red);
+	ledGreenSlider.noUiSlider.set(green);
+	ledBlueSlider.noUiSlider.set(blue);
+	ledWhiteSlider.noUiSlider.set(white);
+}
+
+
+
+
+//-------------------------END LEDS MANUAL
+
+
+
+/*
+
+
+var resultElement = document.getElementById('result');
+var sliders = document.getElementsByClassName('sliders');
+var colors = [0,0,0];
+
+[].slice.call(sliders).forEach(function( slider, index ){
+		noUiSlider.create(slider, {
+		start: 127,
+		connect: [true, false],
+		orientation: "vertical",
+		range: {
+			'min': 0,
+			'max': 255
+	},
+	format: wNumb({
+		decimals: 0
+	})
+	});
+	
+	// Bind the color changing function to the update event.
+	slider.noUiSlider.on('update', function ( ) {
+	
+	colors[index] = slider.noUiSlider.get();
+	
+	var color = 'rgb(' + colors.join(',') + ')';
+
+	resultElement.style.background = color;
+	resultElement.style.color = color;
+	});
+});
+
+*/
+
+
+
 
